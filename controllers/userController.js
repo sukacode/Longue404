aab0c54e51585741433b54d8f46bde2b5a3f5d0c
sukacode <firmansahalhadi@gmail.com>
2022-07-25 10:48:23 +0700

    change userController and routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const user = require("../models/user");
 const User = require("../models/user");
 
 class UserController {
@@ -14,7 +15,78 @@ class UserController {
       return res.status(201).json({
         data: newUser,
         status: "success",
-        message: "Succesfully create User",
+        message: "Succesfully Register",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async findAllUser(req, res) {
+    try {
+      const data = await User.find({}).exec();
+
+      return res
+        .status(200)
+        .json({ data: data, message: "success GET users", status: "success" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async findById(req, res) {
+    try {
+      const id = req.params.id;
+
+      const data = await User.findById(id).exec();
+
+      return res.status(200).json({
+        data: data,
+        message: "success Get user by id",
+        status: success,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async updateUser(req, res) {
+    try {
+      // cari By id
+      // cari data yang dicari, itu baru di update
+      // di save
+
+      const id = req.params.id;
+      const data = await User.findById(id).exec();
+
+      // update data
+      data["usernmae"] = req.body.username ? req.body.username : data.username;
+      data["email"] = req.body.email ? req.body.email : data.email;
+      data["updated_at"] = new Date().toISOString;
+
+      const updatedData = await data.save();
+
+      return res.status(200).json({
+        data: updatedData,
+        message: "success update user",
+        status: "success,",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async deleteUser(req, res) {
+    try {
+      const is = req.params.id;
+      const data = await user.findById(id).exec();
+
+      const deleted = await data.remove();
+
+      return res.status(200).json({
+        data: deleted,
+        message: "success delete user",
+        status: "success",
       });
     } catch (error) {
       console.log(error);
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
-const res = require("express/lib/response");
-const UserController = require("../controllers/userController");
 const router = require("express").Router();
+const UserController = require("../controllers/userController");
+// const res = require("express/lib/response");
 
 router.route("/api/v1").get((req, res) => {
   res.send("Connected !");
 });
 
 router.route("/api/v1/register").post(UserController.register);
+
+router.route("/api/v1/users").get(UserController.findAllUser);
+
+router.route("/api/v1/users/:id").get(UserController.findById);
 
 module.exports = router;
